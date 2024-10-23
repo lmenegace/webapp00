c180d383a31bf049d6299adc29eb933f7a539829
lmenegace <2301994@escolas.anchieta.br>
2024-10-23 20:00:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"2301994@escolas.anchieta.br\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
